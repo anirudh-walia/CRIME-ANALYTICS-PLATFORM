f44b30305afa6122289fbd04c53ace7f847bd096
Anirudh Walia <19BCS6127@cuchd.in>
2022-11-18 10:41:02 +0530

    Add files via upload

---
+++ static/mapdata.js
@@ -0,0 +1,266 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "https://simplemaps.com",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    "1": {
+      name: "Andaman And Nicobar",
+      description: "CRIME RATE - 1332 // POPULATION - 380,581",
+      url: "https://ncrb.gov.in/en/crime-india"
+    },
+    "2": {
+      name: "Andhra Pradesh",
+      description: "CRIME RATE - 222199 // POPULATION - 49,577,103",
+      url: "https://ncrb.gov.in/en/crime-india"
+    },
+    "3": {
+      name: "Arunachal Pradesh",
+      description: "CRIME RATE - 2817 // POPULATION - 1,383,727",
+      url: "https://ncrb.gov.in/en/crime-india"
+    },
+    "4": {
+      name: "Assam",
+      description: "CRIME RATE - 120572 // POPULATION - 31,205,576",
+      url: "https://ncrb.gov.in/en/crime-india"
+    },
+    "5": {
+      name: "Bihar",
+      description: "CRIME RATE - 262815 // POPULATION - 104,099,452",
+      url: "https://ncrb.gov.in/en/crime-india"
+    },
+    "6": {
+      name: "Chandigarh",
+      description: "CRIME RATE - 2995 // POPULATION - 1,055,450",
+      url: "https://ncrb.gov.in/en/crime-india"
+    },
+    "7": {
+      name: "Chhattisgarh",
+      description: "CRIME RATE - 110633 // POPULATION - 25,545,198",
+      url: "https://ncrb.gov.in/en/crime-india"
+    },
+    "8": {
+      name: "Dadra And Nagar Haveli",
+      description: "CRIME RATE - 579\t// POPULATION - 585,764",
+      url: "https://ncrb.gov.in/en/crime-india"
+    },
+    "9": {
+      name: "Daman And Diu",
+      description: "CRIME RATE - 579\t// POPULATION - 585,764",
+      url: "https://ncrb.gov.in/en/crime-india"
+    },
+    "10": {
+      name: "Delhi",
+      description: "CRIME RATE - 306389 // POPULATION - 32,066,000",
+      url: "https://delhipolice.gov.in/statistics"
+    },
+    "11": {
+      name: "Goa",
+      description: "CRIME RATE - 2991// POPULATION - 1,458,545",
+      url: "https://ncrb.gov.in/en/crime-india"
+    },
+    "12": {
+      name: "Gujarat",
+      description: "CRIME RATE - 731738 // POPULATION - 60,439,692",
+      url: "https://ncrb.gov.in/en/crime-india"
+    },
+    "13": {
+      name: "Haryana",
+      description: "CRIME RATE - 206431 // POPULATION - 25,351,462",
+      url: "https://ncrb.gov.in/en/crime-india"
+    },
+    "14": {
+      name: "Himachal Pradesh",
+      description: "CRIME RATE - 18833 // POPULATION - 6,864,602",
+      url: "https://ncrb.gov.in/en/crime-india"
+    },
+    "16": {
+      name: "Jharkhand",
+      description: "CRIME RATE - 60765 // POPULATION - 32,988,134",
+      url: "https://ncrb.gov.in/en/crime-india"
+    },
+    "17": {
+      name: "Karnataka",
+      description: "CRIME RATE - 163697 // POPULATION - 61,095,297",
+      url: "https://ncrb.gov.in/en/crime-india"
+    },
+    "18": {
+      name: "Kerala",
+      description: "CRIME RATE - 524960 // POPULATION - 33,406,061",
+      url: "https://ncrb.gov.in/en/crime-india"
+    },
+    "19": {
+      name: "Lakshadweep",
+      description: "CRIME RATE - 128 // POPULATION - 64,473",
+      url: "https://ncrb.gov.in/en/crime-india"
+    },
+    "20": {
+      name: "Madhya Pradesh",
+      description: "CRIME RATE - 475918 // POPULATION - 72,626,809",
+      url: "https://ncrb.gov.in/en/crime-india"
+    },
+    "21": {
+      name: "Maharashtra",
+      description: "CRIME RATE - 540800 // POPULATION - 112,374,333",
+      url: "https://mumbaipolice.gov.in/CrimeStatistics"
+    },
+    "22": {
+      name: "Manipur",
+      description: "CRIME RATE - 3204 // POPULATION - 2,570,390",
+      url: "https://ncrb.gov.in/en/crime-india"
+    },
+    "23": {
+      name: "Meghalaya",
+      description: "CRIME RATE - 3428 // POPULATION - 2,966,889",
+      url: "https://ncrb.gov.in/en/crime-india"
+    },
+    "24": {
+      name: "Mizoram",
+      description: "CRIME RATE - 3196 // POPULATION - 1,097,206",
+      url: "https://ncrb.gov.in/en/crime-india"
+    },
+    "25": {
+      name: "Nagaland",
+      description: "CRIME RATE - 1478 // POPULATION - 1,978,502",
+      url: "https://ncrb.gov.in/en/crime-india"
+    },
+    "26": {
+      name: "Orissa",
+      description: "CRIME RATE - 155420 // POPULATION - 41,974,219",
+      url: "https://ncrb.gov.in/en/crime-india"
+    },
+    "27": {
+      name: "Puducherry",
+      description: "CRIME RATE - 4595 // POPULATION - 1,247,953",
+      url: "https://ncrb.gov.in/en/crime-india"
+    },
+    "28": {
+      name: "Punjab",
+      description: "CRIME RATE - 73581 // POPULATION - 27,743,338",
+      url: "https://www.punjabpolice.gov.in/tech_wings.aspx"
+    },
+    "29": {
+      name: "Rajasthan",
+      description: "CRIME RATE - 284569 // POPULATION - 68,548,437",
+      url: "https://ncrb.gov.in/en/crime-india"
+    },
+    "30": {
+      name: "Sikkim",
+      description: "CRIME RATE - 728 // POPULATION - 610,577",
+      url: "https://ncrb.gov.in/en/crime-india"
+    },
+    "31": {
+      name: "Tamil Nadu",
+      description: "CRIME RATE - 756753 // POPULATION - 72,147,030",
+      url: "https://ncrb.gov.in/en/crime-india"
+    },
+    "32": {
+      name: "Tripura",
+      description: "CRIME RATE - 4788 // POPULATION - 3,673,917",
+      url: "https://ncrb.gov.in/en/crime-india"
+    },
+    "33": {
+      name: "Uttar Pradesh",
+      description: "CRIME RATE - 608082 // POPULATION - 199,812,341",
+      url: "https://ncrb.gov.in/en/crime-india"
+    },
+    "34": {
+      name: "Uttrakhand",
+      description: "CRIME RATE - 34875 // POPULATION - 10,086,292",
+      url: "https://ncrb.gov.in/en/crime-india"
+    },
+    "35": {
+      name: "West Bengal",
+      description: "CRIME RATE - 181821 // POPULATION - 91,276,115",
+      url: "https://ncrb.gov.in/en/crime-india"
+    },
+    "36": {
+      name: "Jammu And Kashmir",
+      description: "CRIME RATE - 31675 // POPULATION - 12,267,032",
+      url: "https://ncrb.gov.in/en/crime-india"
+    },
+    "37": {
+      name: "Telangana",
+      description: "CRIME RATE - 158809 // POPULATION - 35,003,674",
+      url: "https://ncrb.gov.in/en/crime-india"
+    },
+    "38": {
+      name: "Ladakh",
+      description: "CRIME RATE - 559 // POPULATION - 274,000",
+      url: "https://ncrb.gov.in/en/crime-india"
+    }
+  },
+
+  labels: {},
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
